fffbbb7abb34670ccfd1948fbd12928ad15e3a55
Azel4231 <Azel4231@users.noreply.github.com>
2018-10-17 18:36:50 +0200

    Cleaner version

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 A naive solver for the eight queens puzzle in Clojure.
 
+Runs for several minutes for a 8x8 chess board.
+
+# How it works
+
+* Enumerates all combinations of queens where each queen already sits in their own row
+* Filters out all combinations that contain queens in the same column and/or diagonals
+
 ## License
 
 MIT
--- src/muc_codingdojo_queens/core.clj
+++ src/muc_codingdojo_queens/core.clj
@@ -9,24 +9,32 @@
 (def empty-board (into [] (repeat dimension empty-line)))
 (p/pprint empty-board)
 
+;; Enumerate all the positions on the board as [x y] tuples
 (def all-positions (for [x (range dimension)]
                      (for [y (range dimension)]
                        [x y])))
 
+;; Use a library to calculate all possible combinations of queen placements, if queens are placed each in their own row.
+;; This means 8 to the power of 8 = 16.7M combinations
 (def all-combinations (apply c/cartesian-product all-positions))
 
-(defn none-in-same-row? [combination]
+;; check for queens in same column
+(defn none-in-same-column? [combination]
   (= dimension (count (distinct (map second combination)))))
 
-(defn none-in-same-column? [combination]
+;; check for queens in same row (not necessary because those combinations are not included)
+(defn none-in-same-row? [combination]
   (= dimension (count (distinct (map first combination)))))
 
+;; check for queens in same diagonal
 (defn none-on-same-diagonal? [combination]
   (= dimension (count (distinct (map (partial apply +) combination)))))
 
+;; check for queens in same other diagonal
 (defn none-on-same-other-diagonal? [combination]
   (= dimension (count (distinct (map (partial apply -) combination)))))
 
+;; combine all rules/predicates to one predicate
 (defn valid? [combination]
   ((every-pred none-in-same-row? 
                none-in-same-column?
@@ -34,6 +42,7 @@
                none-on-same-other-diagonal?)
     combination))
 
+;; filter all 16.7M combinations to find the 92 valid ones
 (def result (filter valid? all-combinations))
 
 (println (count result))
