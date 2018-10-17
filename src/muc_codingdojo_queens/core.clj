ed676517ba092062acd2c49bf17546c441513c0e
Azel4231 <Azel4231@users.noreply.github.com>
2018-10-17 17:43:14 +0200

    Solution coding dojo

--- src/muc_codingdojo_queens/core.clj
+++ src/muc_codingdojo_queens/core.clj
@@ -1,6 +1,37 @@
-(ns muc-codingdojo-queens.core)
+(ns muc-codingdojo-queens.core
+  (:require [clojure.pprint :as p]
+            [clojure.math.combinatorics :as c]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def dimension 8)
+
+(def empty-line (into [] (repeat dimension :_)))
+(def empty-board (into [] (repeat dimension empty-line)))
+
+(def all-positions (for [x (range dimension)]
+                     (for [y (range dimension)]
+                       [x y])))
+
+(def all-combinations (apply c/cartesian-product all-positions))
+
+(defn none-in-same-row? [combination]
+  (= dimension (count (distinct (map second combination)))))
+
+(defn none-in-same-column? [combination]
+  (= dimension (count (distinct (map first combination)))))
+
+(defn none-on-same-diagonal? [combination]
+  (= dimension (count (distinct (map (partial apply +) combination)))))
+
+(defn none-on-same-other-diagonal? [combination]
+  (= dimension (count (distinct (map (partial apply -) combination)))))
+
+(defn valid? [combination]
+  ((every-pred none-in-same-row?
+               none-in-same-column?
+               none-on-same-diagonal?
+               none-on-same-other-diagonal?)
+    combination))
+
+(def result (filter valid? all-combinations))
+
+(println (count result))
